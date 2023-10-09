4fb99815afe28d0f21749e3fe219fea2c702b342
Simon Cole <simonccole1@gmail.com>
2023-10-09 14:22:25 -0400

    added array implementatation and tests

--- src/App.java
+++ src/App.java
@@ -1,5 +1,75 @@
+import java.util.EmptyStackException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        testArrayStack();
+
+        
     }
+    public static void testArrayStack() {
+    GenericStack<Integer> stack = new GenericStack<>(3);
+    
+    // test pushing and popping integers
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    assert stack.pop() == 3;
+    assert stack.pop() == 2;
+    assert stack.pop() == 1;
+    assert stack.isEmpty();
+    
+    // test pushing and popping null values
+    stack.push(null);
+    assert stack.pop() == null;
+    assert stack.isEmpty();
+    
+    // test pushing and popping strings
+    GenericStack<String> stringStack = new GenericStack<>();
+    stringStack.push("hello");
+    stringStack.push("world");
+    assert stringStack.pop().equals("world");
+    assert stringStack.pop().equals("hello");
+    assert stringStack.isEmpty();
+    
+    // test pushing and popping doubles
+    GenericStack<Double> doubleStack = new GenericStack<>();
+    doubleStack.push(3.14);
+    doubleStack.push(2.71);
+    assert doubleStack.pop() == 2.71;
+    assert doubleStack.pop() == 3.14;
+    assert doubleStack.isEmpty();
+    
+    // test pushing more elements than the initial capacity
+    for (int i = 0; i < 10; i++) {
+        stack.push(i);
+    }
+    assert stack.pop() == 9;
+    assert stack.pop() == 8;
+    assert stack.pop() == 7;
+    assert stack.pop() == 6;
+    assert stack.pop() == 5;
+    assert stack.pop() == 4;
+    assert stack.pop() == 3;
+    assert stack.pop() == 2;
+    assert stack.pop() == 1;
+    assert stack.pop() == 0;
+    assert stack.isEmpty();
+    
+    // test popping from an empty stack
+    try {
+        stack.pop();
+        assert false;
+    } catch (EmptyStackException e) {
+        assert true;
+    }
+    
+    // test checking if an empty stack is empty
+    assert stack.isEmpty();
+    
+    // test checking if a non-empty stack is empty
+    stack.push(1);
+    assert !stack.isEmpty();
+    
+    System.out.println("All tests passed!");
+}
 }
--- src/GenericStack.java
+++ src/GenericStack.java
@@ -1,25 +1,67 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
 public class GenericStack<E> {
-    private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+    private final static int DEFAULT_CAPACITY = 50;
+    private E[] list;
+    private int capacity;
+    private int size;
+
+
+    public GenericStack() {
+        list =  (E[]) new Object [DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+    }
+
+    public GenericStack(int capacity) {
+        list =  (E[]) new Object [capacity];
+        this.capacity = capacity;
+    }
     
     public int getSize() {
-        return list.size();
+        return size;
     }
 
     public void push(E o) {
-        list.add(o);
+        //check size, if full create new arr that is double the size and copy
+        if(size == capacity) {
+            capacity *= 2;
+            E[] newList = (E[]) new Object[capacity];
+            System.arraycopy(list, 0, newList, 0, capacity / 2);
+            list = newList;
+        }
+        list[size] = o;
+        size++;
     }
 
     public E pop() {
-        E o = list.get(getSize() - 1);
-        return o;
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        E element = list[size - 1];
+        E[] newList = Arrays.copyOf(list, capacity - 1);
+        list = newList;
+        capacity--;
+        size--;
+        return element;
     }
 
     public boolean isEmpty() {
-        return list.isEmpty();
+       return size == 0 ? true : false;
     }
 
     @Override
     public String toString() {
-        return "stack: " + list.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(list[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
